927d2da7099a0ca1c69b0ecf65e8111bcf98ba9e
Swapnil Nadekar <swapnil.n@hofintech.com>
2024-09-06 11:59:59 +0530

    user context changed at the frontend in web

--- apps/web/src/app/contexts/user-context.ts
+++ apps/web/src/app/contexts/user-context.ts
@@ -1,34 +1,36 @@
-import { Hospital } from "@healthcare/data-transfer-types";
+import { Hospital, ViewUser } from "@healthcare/data-transfer-types";
 import {  HospitalRoleName, SuperRoleName } from "@prisma/client";
 import React from "react";
 // import {  User  } from "@healthcare/data-transfer-types";
 
 
 
-interface User {
-    id: number;
-    firstName: string;
-    lastName: string;
-    email: string;
-    phoneNumber: string;
-    hospitalRoles: {
-        hospitalId: number;
-        hospitalName: string;
-        hospitalRole: string;
-    }[],
-    organizationRoles?: OrganizationRoleDto[];
-    superRole?: SuperRoleName;
-}
+// interface User {
+//     id: number;
+//     firstName: string;
+//     lastName: string;
+//     email: string;
+//     phoneNumber: string;
+//     hospitalRoles: {
+//         hospitalId: number;
+//         hospitalName: string;
+//         hospitalRole: string;
+//     }[],
+//     organizationRoles?: OrganizationRoleDto[];
+//     superRole?: SuperRoleName;
+//     doctorId?:number;
+//     patientId?:number;
+// }
 
 
 
-export interface OrganizationRoleDto {
-    organizationId: number;
-    organizationRole: HospitalRoleName;
-}
+// export interface OrganizationRoleDto {
+//     organizationId: number;
+//     organizationRole: HospitalRoleName;
+// }
 
 
-export const UserContext = React.createContext<User | null>(null);
+export const UserContext = React.createContext<ViewUser | null>(null);
 export const HospitalContext=React.createContext<Hospital | null>(null);
 
 export default UserContext;
