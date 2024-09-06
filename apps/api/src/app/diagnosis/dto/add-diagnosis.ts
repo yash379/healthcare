f7951f9318973ef28d7b3979b5d0f0198cd5f9d6
Omkarkhadake <omkarsatishkhadke132001@gmail.com>
2024-09-06 12:51:44 +0530

    omkar :dianosis file detele

--- apps/api/src/app/diagnosis/dto/add-diagnosis.ts
+++
@@ -1,5 +0,0 @@
-import { AddDiagnosis } from "@healthcare/data-transfer-types";
-import { OmitType } from "@nestjs/swagger";
-import { DiagnosisDto } from "./diagnosis.dto";
-
-export class AddDoctorDto extends OmitType(DiagnosisDto,['id']) implements AddDiagnosis {}
--- apps/api/src/app/diagnosis/dto/list-diagnosis-page.dto.ts
+++
@@ -1,8 +0,0 @@
-import { ListDiagnosis, ListDiagnosisPage } from "@healthcare/data-transfer-types";
-import { ApiProperty } from "@nestjs/swagger";
-import { PageBaseDto } from "../../core/dto/page-base.dto";
-import { ListDiagnosisDto } from "./list-diagnosis.dto";
-
-export class ListDiagnosisPageDto extends PageBaseDto implements ListDiagnosisPage{
-    @ApiProperty({type:[ListDiagnosisDto]}) content: ListDiagnosisDto[];
-}
