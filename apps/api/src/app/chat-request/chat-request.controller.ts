02798b1aef1ea66fb9c1ae3643eca61aab5e80b6
yhofin <yash.c@hofintech.com>
2024-09-15 18:49:43 +0530

    upload api for saransnai

--- apps/api/src/app/chat-request/chat-request.controller.ts
+++ apps/api/src/app/chat-request/chat-request.controller.ts
@@ -1,19 +1,36 @@
-import { Controller, Post, Body } from '@nestjs/common';
-import { ApiBody, ApiTags } from '@nestjs/swagger';
-import { ChatRequestDto } from './dto/chat-request.dto'; // Adjust the path if necessary
+import { Body, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiTags, ApiConsumes, ApiBody } from '@nestjs/swagger';
 import { ChatRequestService } from './chat-request.service';
+import { Express } from 'express';
+import { ChatRequestDto } from './dto/chat-request.dto';
 
 @ApiTags('chat')
-@Controller('chat-request')  // Add the @Controller() decorator with a path
+@Controller('chat-request')
 export class ChatRequestController {
   constructor(private readonly chatRequestService: ChatRequestService) {}
 
   @Post('chat')
-  @ApiBody({
-    description: 'Chat request payload',
-    type: ChatRequestDto,
-  })
   async chat(@Body() body: ChatRequestDto): Promise<any> {
     return this.chatRequestService.chat(body.text);
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    description: 'File upload',
+    schema: {
+      type: 'object',
+      properties: {
+        file: {
+          type: 'string',
+          format: 'binary',
+        },
+      },
+    },
+  })
+  async uploadFile(@UploadedFile() file: Express.Multer.File): Promise<any> {
+    return this.chatRequestService.uploadFile(file);
   }
 }
