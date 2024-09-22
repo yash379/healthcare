ca5d37dfed5fc00ec8ab9d736fec8fc9c7b8f71d
swapnilnhofintech <swapnil.n@hofintech.com>
2024-09-22 21:51:24 +0530

    replaced society with hospital

--- apps/web/src/app/pages/list-hospitals/edit-hospital/edit-hospital.spec.tsx
+++ apps/web/src/app/pages/list-hospitals/edit-hospital/edit-hospital.spec.tsx
@@ -1,10 +1,14 @@
 import { render } from '@testing-library/react';
 
-import EditSociety from './edit-hospital';
+import EditHospital, { AddHospital } from './edit-hospital';
 
-describe('EditSociety', () => {
+describe('EditHospital', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<EditSociety />);
+    const { baseElement } = render(<EditHospital open={false} onClose={function (): void {
+      throw new Error('Function not implemented.');
+    } } onUpdate={function (data: AddHospital): void {
+      throw new Error('Function not implemented.');
+    } } initialData={null} />);
     expect(baseElement).toBeTruthy();
   });
 });
